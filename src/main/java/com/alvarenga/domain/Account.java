d2d206d67ea935866c7c923756a9f335ba925d4e
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-09-27 14:39:25 -0300

    feat: add validation to account class

--- src/main/java/com/alvarenga/domain/Account.java
+++ src/main/java/com/alvarenga/domain/Account.java
@@ -1,14 +1,25 @@
 package com.alvarenga.domain;
 
+import com.alvarenga.domain.exceptions.ValidationException;
+
 public class Account {
   private Long id;
   private String name;
   private User user;
 
   public Account(Long id, String name, User user) {
+    validate(name, user);
+
     this.id = id;
     this.name = name;
     this.user = user;
+  }
+
+  public void validate(String name, User user) {
+    if (name == null || name.isEmpty())
+      throw new ValidationException("Name cannot be null or empty");
+    if (user == null)
+      throw new ValidationException("User cannot be null");
   }
 
   public Long getId() {
--- src/test/java/domain/TestAccount.java
+++ src/test/java/domain/TestAccount.java
@@ -1,9 +1,11 @@
 package domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.alvarenga.domain.Account;
+import com.alvarenga.domain.exceptions.ValidationException;
 
 import domain.builders.AccountBuilder;
 import domain.builders.UserBuilder;
@@ -11,6 +13,7 @@ import domain.builders.UserBuilder;
 public class TestAccount {
 
   @Test
+  @DisplayName("Should create a valid account")
   public void shouldCreateAValidAccount() {
     Account account = AccountBuilder.oneAccount().build();
 
@@ -19,5 +22,35 @@ public class TestAccount {
         () -> Assertions.assertEquals(1L, account.getId()),
         () -> Assertions.assertEquals("Valid name", account.getName()),
         () -> Assertions.assertEquals(UserBuilder.oneUser().build(), account.getUser()));
+  }
+
+  @Test
+  @DisplayName("Should throw a ValidationException if name is null")
+  public void shouldThrowIfNameIsNull() {
+    ValidationException exception = Assertions.assertThrows(
+        ValidationException.class,
+        () -> AccountBuilder.oneAccount().withName(null).build());
+
+    Assertions.assertEquals("Name cannot be null or empty", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("Should throw a ValidationException if name is empty")
+  public void shouldThrowIfNameIsEmpty() {
+    ValidationException exception = Assertions.assertThrows(
+        ValidationException.class,
+        () -> AccountBuilder.oneAccount().withName("").build());
+
+    Assertions.assertEquals("Name cannot be null or empty", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("Should throw a ValidationException if user is null")
+  public void shouldThrowIfUserIsNull() {
+    ValidationException exception = Assertions.assertThrows(
+        ValidationException.class,
+        () -> AccountBuilder.oneAccount().withUser(null).build());
+
+    Assertions.assertEquals("User cannot be null", exception.getMessage());
   }
 }
