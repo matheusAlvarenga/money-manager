46255b4e7c1b97f29a8546fe53dcc990cc43ffe6
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-10-06 09:10:29 -0300

    refactor: add setup to before each

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.any;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -16,23 +17,23 @@ import domain.builders.UserBuilder;
 
 public class UserServiceTest {
   private UserService userService;
+  private UserRepository repository;
+
+  @BeforeEach
+  public void setup() {
+    repository = Mockito.mock(UserRepository.class);
+
+    userService = new UserService(repository);
+  }
 
   @Test
   public void shouldReturnEmptyIfUserIsUnexistent() {
-    UserRepository repository = Mockito.mock(UserRepository.class);
-
-    userService = new UserService(repository);
-
     Optional<User> user = userService.getUserByEmail("unexistent_email@mail.com");
     Assertions.assertTrue(user.isEmpty());
   }
 
   @Test
   public void shouldReturnUserIfUserIsExistent() {
-    UserRepository repository = Mockito.mock(UserRepository.class);
-
-    userService = new UserService(repository);
-
     Mockito.when(repository.getUserByEmail(any())).thenReturn(Optional.of(UserBuilder.oneUser().build()));
 
     Optional<User> user = userService.getUserByEmail("unexistent_email@mail.com");
@@ -41,11 +42,7 @@ public class UserServiceTest {
 
   @Test
   public void shouldCreateUserWithSuccess() {
-    UserRepository repository = Mockito.mock(UserRepository.class);
-
     Mockito.when(repository.save(any())).thenReturn(UserBuilder.oneUser().build());
-
-    userService = new UserService(repository);
 
     User userToSave = UserBuilder.oneUser().withId(null).build();
 
