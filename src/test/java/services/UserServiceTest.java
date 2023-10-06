1fdcde68726b98e0873e03a4bca0567842b23f1f
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-10-06 09:03:47 -0300

    test: test creating user

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -38,4 +38,21 @@ public class UserServiceTest {
     Optional<User> user = userService.getUserByEmail("unexistent_email@mail.com");
     Assertions.assertTrue(user.isPresent());
   }
+
+  @Test
+  public void shouldCreateUserWithSuccess() {
+    UserRepository repository = Mockito.mock(UserRepository.class);
+
+    Mockito.when(repository.save(any())).thenReturn(UserBuilder.oneUser().build());
+
+    userService = new UserService(repository);
+
+    User userToSave = UserBuilder.oneUser().withId(null).build();
+
+    User savedUser = userService.save(userToSave);
+
+    Assertions.assertNotNull(savedUser.getId());
+
+    Mockito.verify(repository).getUserByEmail(userToSave.getEmail());
+  }
 }
